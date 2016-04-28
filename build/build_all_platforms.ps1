8106844ef5586b953b0219a4bb804523711cac2c
Mikael Hedberg <mihedb@gmail.com>
2016-04-28 11:45:19 +0200

    - Fix bugs in build scripts

--- build/build_all_platforms.ps1
+++ build/build_all_platforms.ps1
@@ -1,7 +1,22 @@
 $vs_version = $args[0]
 $vs_path = $args[1]
 
-Invoke-Expression build_v8.ps1 Debug ia32 $vs_version $vs_path
-Invoke-Expression build_v8.ps1 Release ia32 $vs_version $vs_path
-Invoke-Expression build_v8.ps1 Debug x64 $vs_version $vs_path
-Invoke-Expression build_v8.ps1 Release x64 $vs_version $vs_path
+If(!$vs_version)
+{
+	$vs_version = "2013"
+	Write-Host "Using the default visual studio version: "$vs_version
+}
+If(!$vs_path)
+{
+	Invoke-Expression (".\build_v8.ps1 Debug ia32 " + $vs_version)
+	Invoke-Expression (".\build_v8.ps1 Release ia32 " + $vs_version)
+	Invoke-Expression (".\build_v8.ps1 Debug x64 " + $vs_version)
+	Invoke-Expression (".\build_v8.ps1 Release x64 " + $vs_version)	
+}
+Else
+{
+	Invoke-Expression (".\build_v8.ps1 Debug ia32 " + $vs_version + " " + $vs_path)
+	Invoke-Expression (".\build_v8.ps1 Release ia32 " + $vs_version + " " + $vs_path)
+	Invoke-Expression (".\build_v8.ps1 Debug x64 " + $vs_version + " " + $vs_path)
+	Invoke-Expression (".\build_v8.ps1 Release x64 " + $vs_version + " " + $vs_path)
+}
--- build/get_build_everything.ps1
+++ build/get_build_everything.ps1
@@ -3,6 +3,36 @@ $tag = $args[1]
 $vs_path = $args[2]
 $depot_tools_url = $args[3]
 
-Invoke-Expression get_v8.ps1 $tag $depot_tools_url 
-Invoke-Expression build_all_platforms.ps1 $vs_version $vs_path
+If($depot_tools_url -and $tag)
+{
+	Invoke-Expression (".\get_v8.ps1 " + $tag + " " + $depot_tools_url )
+}
+ElseIf($tag)
+{
+	Invoke-Expression (".\get_v8.ps1 " + $tag)
+}
+ElseIf($depot_tools_url)
+{
+	Write-Host "Invalid parameters"
+	return;
+}
+Else
+{
+	Invoke-Expression ".\get_v8.ps1"
+}
+
+If(!$vs_version)
+{
+	$vs_version = "2013"
+	Write-Host "Using the default visual studio version: "$vs_version
+}
+If($vs_path)
+{
+	Invoke-Expression (".\build_all_platforms.ps1 " + $vs_version + " " + $vs_path)
+}
+Else
+{
+	Invoke-Expression (".\build_all_platforms.ps1 " + $vs_version)
+}
+
 
